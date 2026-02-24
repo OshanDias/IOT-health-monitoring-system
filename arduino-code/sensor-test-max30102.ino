1e88ca9b71c02d1c852a0343c08d2a1843a68a0d
shakya122 <shakyasithmini@gmail.com>
2026-02-24 14:48:34 +0530

    Update sensor-test-max30102.ino

--- arduino-code/sensor-test-max30102.ino
+++ arduino-code/sensor-test-max30102.ino
@@ -1,1 +1,25 @@
 ## Basic Arduino sketch
+#include <Wire.h>
+#include "MAX30105.h" // Common library for MAX30102
+
+MAX30105 particleSensor;
+
+void setup() {
+  Serial.begin(115200);
+  if (!particleSensor.begin(Wire, I2C_SPEED_STANDARD)) {
+    Serial.println("MAX30102 not found!");
+    while (1);
+  }
+  particleSensor.setup(); // Configure sensor with default settings
+}
+
+void loop() {
+  // Read raw values
+  long irValue = particleSensor.getIR();
+  long redValue = particleSensor.getRed();
+
+  Serial.print("IR: "); Serial.print(irValue);
+  Serial.print(" | Red: "); Serial.println(redValue);
+  
+  delay(100); 
+}
